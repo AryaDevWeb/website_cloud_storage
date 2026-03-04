264c865b3a79d28af77f9d47c0b662e09f8308b7
RaffiKece2 <keceraffi2@gmail.com>
2026-03-05 06:28:57 +0700

    memperbaiki error file

--- resources/views/lihat.blade.php
+++ resources/views/lihat.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $file->file }}</title>
+    <title>{{ $file->nama_tampilan }}</title>
 </head>
 <body>
     <form action="/beranda/{{ auth()->id() }}">
         <button>Beranda</button>
     </form>
-    <h1>{{ $file->file }}</h1>
+    <h1>{{ $file->nama_tampilan }}</h1>
 
     <p>{!!   nl2br(e($teks)) !!}</p>
     
